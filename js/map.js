4ff54946b6efe1a83778690bd5130d825e4cf5d7
Kazunori OYA <terje@oya3.net>
2015-05-06 23:48:49 +0900

    transitive.js が出力している画像が表示できたが理由が不明

--- js/map.js
+++ js/map.js
@@ -29,14 +29,12 @@ CoordMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
 };
 
 var overlay;
-USGSOverlay.prototype = new google.maps.OverlayView();
+TransitiveOverlay.prototype = new google.maps.OverlayView();
 
 /** @constructor */
-function USGSOverlay(bounds, image, map) {
+function TransitiveOverlay(map) {
 
   // Initialize all properties.
-  this.bounds_ = bounds;
-  this.image_ = image;
   this.map_ = map;
 
   // Define a property to hold the image's div. We'll
@@ -52,39 +50,35 @@ function USGSOverlay(bounds, image, map) {
  * onAdd is called when the map's panes are ready and the overlay has been
  * added to the map.
  */
-// var d3 = require('d3');
-// var Transitive = require('transitive');
-// var OtpProfiler = require('otp-profiler');
-
-USGSOverlay.prototype.onAdd = function() {
+TransitiveOverlay.prototype.onAdd = function() {
   
-  // 下は画像を表示するやつ。
   var div = document.createElement('div');
+  this.div_ = div;
   div.style.borderStyle = 'none';
   div.style.borderWidth = '0px';
   div.style.position = 'absolute';
 
-  // Create the img element and attach it to the div.
   var img = document.createElement('img');
-  img.src = this.image_;
+  img.src = 'https://developers.google.com/maps/documentation/javascript/images/control-positions.png';
   img.style.width = '100%';
   img.style.height = '100%';
   img.style.position = 'absolute';
   div.appendChild(img);
 
+  
   var transitive_div = document.createElement('div');
-  transitive_div.style.backgroundColor  = 'transparent';
+  // transitive_div.style.backgroundColor  = 'transparent';
   transitive_div.style.position = 'absolute';
   transitive_div.style.top = '0';
   transitive_div.style.left = '0';
   transitive_div.style.height = '100%';
   transitive_div.style.width = '100%';
-//  margin-right: 400px;
   div.appendChild(transitive_div);
-  
-  this.div_ = div;
 
+  var d3 = require('d3');
   var Transitive = require('transitive');
+  var OtpProfiler = require('otp-profiler');
+  
   this.transitive = new Transitive({
     el: transitive_div,
     // これは路線説明欄を表示するためにある
@@ -109,31 +103,31 @@ USGSOverlay.prototype.onAdd = function() {
   panes.overlayLayer.appendChild(div);
 };
 
-USGSOverlay.prototype.draw = function() {
+TransitiveOverlay.prototype.draw = function() {
   this.transitive.render();
 
   // We use the south-west and north-east
   // coordinates of the overlay to peg it to the correct position and size.
   // To do this, we need to retrieve the projection from the overlay.
-  var overlayProjection = this.getProjection();
+//  var overlayProjection = this.getProjection();
 
   // Retrieve the south-west and north-east coordinates of this overlay
   // in LatLngs and convert them to pixel coordinates.
   // We'll use these coordinates to resize the div.
-  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+//  var sw = overlayProjection.fromLatLngToDivPixel(this.div_.getSouthWest());
+//  var ne = overlayProjection.fromLatLngToDivPixel(this.div_.getNorthEast());
 
   // Resize the image's div to fit the indicated dimensions.
   var div = this.div_;
-  div.style.left = sw.x + 'px';
-  div.style.top = ne.y + 'px';
-  div.style.width = (ne.x - sw.x) + 'px';
-  div.style.height = (sw.y - ne.y) + 'px';
+  div.style.left = '0px';
+  div.style.top = '0px';
+  div.style.width = '300px';
+  div.style.height = '300px';
 };
 
 // The onRemove() method will be called automatically from the API if
 // we ever set the overlay's map property to 'null'.
-USGSOverlay.prototype.onRemove = function() {
+TransitiveOverlay.prototype.onRemove = function() {
   this.div_.parentNode.removeChild(this.div_);
   this.div_ = null;
 };
@@ -155,16 +149,16 @@ function initialize() {
   // their parent base map.
   map.overlayMapTypes.insertAt(0, new CoordMapType(new google.maps.Size(256, 256)));
 
-  var swBound = new google.maps.LatLng(62.281819, -150.287132);
-  var neBound = new google.maps.LatLng(62.400471, -150.005608);
-  var bounds = new google.maps.LatLngBounds(swBound, neBound);
-  // The photograph is courtesy of the U.S. Geological Survey.
-  var srcImage = 'https://developers.google.com/maps/documentation/javascript/';
-  srcImage += 'examples/full/images/talkeetna.png';
+  // var swBound = new google.maps.LatLng(62.281819, -150.287132);
+  // var neBound = new google.maps.LatLng(62.400471, -150.005608);
+  // var bounds = new google.maps.LatLngBounds(swBound, neBound);
+  // // The photograph is courtesy of the U.S. Geological Survey.
+  // var srcImage = 'https://developers.google.com/maps/documentation/javascript/';
+  // srcImage += 'examples/full/images/talkeetna.png';
 
-  // The custom USGSOverlay object contains the USGS image,
+  // The custom TransitiveOverlay object contains the Transitive image,
   // the bounds of the image, and a reference to the map.
-  overlay = new USGSOverlay(bounds, srcImage, map);
+  overlay = new TransitiveOverlay(map);
 
   // ドラッグイベントでの地図表示位置（センター）
   google.maps.event.addListener(map, "drag", function() {
